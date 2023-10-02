3865f947a9df263dd9d779a5b25d0f49932f1f2c
swalha1999 <mohamd514.m@gmail.com>
2023-10-02 11:01:36 +0300

    change functions name to more verbose ones

--- scripts/about.js
+++ scripts/about.js
@@ -2,7 +2,9 @@ import { println, scanf_promise, printnln } from "./util.js";
 
 
 export async function printName() {
-    println("What is your name?");
-    const name = await scanf_promise();
-    printnln(`Hello, ${name}`);
+    for (let i = 0; i < 3; i++) {    
+        println("What is your name?");
+        const name = await scanf_promise();
+        println(`Hello, ${name}`);
+    }
 }
